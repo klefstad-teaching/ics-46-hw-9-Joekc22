3eb55d6e58f5476a18523ab49115dd9501c6f17f
Joekc22 <joekc1@uci.edu>
2025-03-17 06:28:02 +0000

    didt

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,14 +1,14 @@
 #include "dijkstras.h"
 
 int main(){
-    Graph graph;
-    file_to_graph("src/small.txt", graph);
+    Graph G;
+    file_to_graph("src/small.txt", G);
     int source = 0;
     vector<int> previous;
-    vector<int> dist = dijkstra_shortest_path(graph, source, previous);
+    vector<int> dist = dijkstra_shortest_path(G, source, previous);
 
 
-    for (int target = 0; target < graph.numVertices; ++target){
+    for (int target = 0; target < G.numVertices; ++target){
         vector<int> s_path = extract_shortest_path(dist, previous, target);
         print_path(s_path, dist[target]);
     }
